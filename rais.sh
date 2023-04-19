ecda66819f2e34d5d61fd58ec8abe62f35d6a575
lex <aibangjuxin@qq.com>
2023-04-19 22:55:56 +0800

    This is for my iPhone git push or pull at Wed Apr 19 22:55:56 CST 2023

---
+++ rais.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Replace with the path to your Git repository
+
+# Add all changes to the staging area
+git add .
+
+# Commit the changes with a commit message
+git commit -m "Your commit message here"
+
+# Push the changes to the remote repository
+git push
+
+
