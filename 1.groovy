56ec262511b5a6429e303cfe4fe385ec85dbc4cd
aibangjuxin <aibangjuxin@gmail.com>
2023-04-21 18:08:30 +0800

    更新 1.groovy

--- 1.groovy
+++ 1.groovy
@@ -1,1 +1,26 @@
-a
+
+如果您想使用GKE网络策略（Network Policy）来允许对某个IP段的egress（出口）流量进行限制，但想要排除某个特定的IP地址，可以尝试使用以下规则：
+
+```yaml
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  name: allow-egress
+spec:
+  podSelector: {}
+  policyTypes:
+  - Egress
+  egress:
+  - to:
+    - ipBlock:
+        cidr: <IP段的CIDR>
+        except:
+        - <要排除的IP地址>
+    ports:
+    - protocol: TCP
+      port: <端口号>
+```
+
+在这个规则中，您可以使用 `ipBlock` 来指定要允许的IP段，使用 `except` 来指定要排除的IP地址。这将使得您的网络策略在针对特定IP段的egress流量时，排除掉您指定的IP地址。 
+
+请注意，在您的规则中使用的IP段和IP地址应该遵循CIDR（无类域间路由选择）的格式。
