8e75bfc988a38f522272f70e7392b89cec09e582
aibangjuxin <aibangjuxin@gmail.com>
2023-05-17 16:05:57 +0800

    更新 1.groovy

--- 1.groovy
+++ 1.groovy
@@ -1,52 +1,32 @@
 
-User: 1. 128.0.0.0/2 IP range is 128.0.0.1 to 191.255.255.254
-2. We need to exclude IP address 130.208.127.15 from this range
-3. To express the full IP range from 130.208.127.15 to 191.255.255.254
-4. by dividing the large CIDR block of 128.0.0.0/2 into multiple smaller CIDR blocks
-5. each small CIDR block does not contain the IP address 130.208.127.15
+Here is the English translation:
 
-To exclude the IP address 130.208.127.15 from the IP range 128.0.0.0/2 while expressing the full IP range from 130.208.127.15 to 191.255.255.254, we can divide the large CIDR block of 128.0.0.0/2 into smaller CIDR blocks that don't contain 130.208.127.15. Here are the smaller CIDR blocks:
+Yes, using !CIDR prefix to exclude certain IPs in GKE's nonMasqueradeCIDRs configuration is a supported syntax. 
 
-1. 128.0.0.0/9 (128.0.0.1 to 128.255.255.254)
-2. 129.0.0.0/16 (129.0.0.1 to 129.255.255.254)
-3. 130.0.0.0/17 (130.0.0.1 to 130.127.255.254)
-4. 130.128.0.0/18 (130.128.0.1 to 130.191.255.254)
-5. 130.192.0.0/19 (130.192.0.1 to 130.223.255.254)
-6. 130.224.0.0/20 (130.224.0.1 to 130.239.255.254)
-7. 130.240.0.0/21 (130.240.0.1 to 130.247.255.254)
-8. 130.248.0.0/22 (130.248.0.1 to 130.251.255.254)
-9. 130.252.0.0/23 (130.252.0.1 to 130.253.255.254)
-10. 130.254.0.0/24 (130.254.0.1 to 130.254.255.254)
-11. 130.255.0.0/25 (130.255.0.1 to 130.255.127.254)
-12. 130.255.128.0/26 (130.255.128.1 to 130.255.191.254)
-13. 130.255.192.0/27 (130.255.192.1 to 130.255.223.254)
-14. 130.255.224.0/28 (130.255.224.1 to 130.255.239.254)
-15. 130.255.240.0/29 (130.255.240.1 to 130.255.247.254)
-16. 130.255.248.0/30 (130.255.248.1 to 130.255.251.254)
-17. 130.255.252.0/31 (130.255.252.1 to 130.255.253.254)
-18. 130.255.254.0/32 (130.255.254.1 to 130.255.255.254)
-19. 130.208.0.0/20 (130.208.0.1 to 130.208.15.254)
-20. 130.208.16.0/28 (130.208.16.1 to 130.208.16.14)
-21. 130.208.16.16/30 (130.208.16.17 to 130.208.16.18)
-22. 130.208.16.20/32 (130.208.16.21)
-23. 130.208.16.22/31 (130.208.16.23 to 130.208.16.24)
-24. 130.208.16.26/32 (130.208.16.27)
-25. 130.208.16.28/30 (130.208.16.29 to 130.208.16.30)
-26. 130.208.16.32/27 (130.208.16.33 to 130.208.16.62)
-27. 130.208.16.64/26 (130.208.16.65 to 130.208.16.126)
-28. 130.208.16.128/25 (130.208.16.129 to 130.208.16.254)
-29. 130.208.17.0/24 to 130.208.126.0/24 (130.208.17.1 to 130.208.126.254)
-30. 130.208.127.0/28 (130.208.127.1 to 130.208.127.14)
-31. 130.208.127.16/32 (130.208.127.17)
-32. 130.208.127.18/31 (130.208.127.19 to 130.208.127.20)
-33. 130.208.127.22/32 (130.208.127.23)
-34. 130.208.127.24/29 (130.208.127.25 to 130.208.127.30)
-35. 130.208.127.32/27 (130.208.127.33 to 130.208.127.62)
-36. 130.208.127.64/26 (130.208.127.65 to 130.208.127.126)
-37. 130.208.127.128/25 (130.208.127.129 to 130.208.127.254)
-38. 130.208.128.0/17 (130.208.128.1 to 130.208.255.254)
-39. 130.209.0.0/16 to 131.0.0.0/16 (130.209.0.1 to 131.255.255.254)
-40. 132.0.0.0/8 to 191.0.0.0/8 (132.0.0.1 to 191.255.255.254)
+For example:
+
+If your current nonMasqueradeCIDRs is:
+
+nonMasqueradeCIDRs: "10.0.0.0/8" 
+
+
+And you want to exclude 10.0.1.5 so that it can be SNATed, the correct configuration should be:
+
+nonMasqueradeCIDRs: "10.0.0.0/8,!10.0.1.5/32"
+
+
+This way, only 10.0.1.5 will be SNATed, other IPs in 10.0.0.0/8 will still not be SNATed.
+
+Also, if you want to exclude multiple IPs, you can specify multiple !CIDRs, for example:
+
+nonMasqueradeCIDRs: "10.0.0.0/8,!10.0.1.5/32,!10.0.2.10/32" 
+
+
+The above configuration will SNAT 10.0.1.5 and 10.0.2.10, other IPs in 10.0.0.0/8 remain unchanged.
+
+In summary, using the !CIDR prefix to exclude specific IPs in GKE's nonMasqueradeCIDRs configuration so that those IPs can be SNATed is a fully supported syntax. 
+
+Please feel free to use it and exclude the IPs that need SNAT as needed in your actual configuration. Let me know if you have any other questions when configuring and using this feature.
 
 These smaller CIDR blocks cover the IP range from 130.208.127.15 to 191.255.255.254, while excluding the IP address 130.208.127.15.
 
